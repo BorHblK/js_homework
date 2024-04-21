a4530e30df830d920ff820dbad6b7e887b864948
VadymM <borhblk@gmail.com>
2024-04-21 23:01:47 +0300

    start of code

---
+++ age.js
@@ -0,0 +1,14 @@
+
+const ageInput = prompt("Введіть кількість років:");
+
+if (ageInput != null && isNaN(ageInput) && ageInput >= 0)
+    {
+        console.log("ви вели" + ageInput)
+
+        
+
+    }
+else {
+    console.log("Введіть число яке більше або рівно 0")
+}
+
