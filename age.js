0124e7fa4c60429d532cea1af8cb39bbd8c933e3
VadymM <borhblk@gmail.com>
2024-04-21 23:54:50 +0300

    final varik

--- age.js
+++ age.js
@@ -1,14 +1,28 @@
-
-const ageInput = prompt("Введіть кількість років:");
-
-if (ageInput != null && isNaN(ageInput) && ageInput >= 0)
+let continueInput = true;
+//відкриваємо цикл безкінечності :) 
+do {
+    let ageInput = prompt("Введіть кількість років:");
+    // валідаться що значення є позитивним числом
+    if (ageInput !== null && ageInput !== "" && !isNaN(ageInput) && ageInput>=0)
     {
-        console.log("ви вели" + ageInput)
-
         
+        const lastDigit = ageInput % 10;
+        const lastTwoDigits = ageInput % 100;
 
+        let output;
+
+        if ((lastTwoDigits >= 11 && lastTwoDigits <= 14) || (lastDigit === 0) || (lastDigit >= 5 && lastDigit <= 9)) {
+            output = ageInput + " років";
+        } else if (lastDigit === 1) {
+            output = ageInput + " рік";
+        } else {
+            output = ageInput + " роки";
+        }
+        alert(output);                 
     }
 else {
-    console.log("Введіть число яке більше або рівно 0")
+    alert("Введіть число яке більше або рівно 0 ");
 }
-
+//повертаємось на вхід а закриваємо безкінечність
+continueInput = confirm("Чи хочете ви спробувати ще раз?");
+} while (true);
